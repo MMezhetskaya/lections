13b7ef415ae7eb6ee02e6ba7244778a722919645
Alec Povolotskiy <aleksey.povolotskiy@vaimo.com>
2018-02-16 13:01:50 +0200

    redux tutorial

--- react__tutorial/src/actions/PageActions.js
+++ react__tutorial/src/actions/PageActions.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import {
     GET_PHOTOS_REQUEST,
     GET_PHOTOS_FAIL,
@@ -24,32 +26,15 @@ function makeYearPhotos(photos, selectedYear) {
 }
 
 function getMorePhotos(offset, count, year, dispatch) {
-    VK.Api.call('photos.getAll', {extended:1, count: count, offset: offset},(r) => { // eslint-disable-line no-undef
-        try {
-            if (offset <= r.response[0] - count) {
-                offset += 200;
-                photosArr = photosArr.concat(r.response);
-                getMorePhotos(offset,count,year,dispatch);
-            } else {
-                let photos = makeYearPhotos(photosArr, year);
-
-                cached = true;
-
-                dispatch({
-                    type: GET_PHOTOS_SUCCESS,
-                    payload: photos
-                })
+    FB.api(
+        "/me/photos_all",
+        function (response) {
+            debugger;
+            if (response && !response.error) {
+                /* handle the result */
             }
         }
-        catch(e) {
-            dispatch({
-                type: GET_PHOTOS_FAIL,
-                error: true,
-                payload: new Error(e)
-            })
-        }
-
-    })
+    );
 }
 
 export function getPhotos(year) {
@@ -71,3 +56,118 @@ export function getPhotos(year) {
         }
     }
 }
+
+// VK.Api.call('photos.getAll', {extended:1, count: count, offset: offset},(r) => { // eslint-disable-line no-undef
+//     try {
+//         if (offset <= r.response[0] - count) {
+//             offset += 200;
+//             photosArr = photosArr.concat(r.response);
+//             getMorePhotos(offset,count,year,dispatch);
+//         } else {
+//             let photos = makeYearPhotos(photosArr, year);
+//
+//             cached = true;
+//
+//             dispatch({
+//                 type: GET_PHOTOS_SUCCESS,
+//                 payload: photos
+//             })
+//         }
+//     }
+//     catch(e) {
+//         dispatch({
+//             type: GET_PHOTOS_FAIL,
+//             error: true,
+//             payload: new Error(e)
+//         })
+//     }
+//
+// })
+//
+//
+// function makeFacebookPhotoURL( id, accessToken ) {
+//     return 'https://graph.facebook.com/' + id + '/picture?access_token=' + accessToken;
+// }
+// function login( callback ) {
+//     FB.login(function(response) {
+//         if (response.authResponse) {
+//             //console.log('Welcome!  Fetching your information.... ');
+//             if (callback) {
+//                 callback(response);
+//             }
+//         } else {
+//             console.log('User cancelled login or did not fully authorize.');
+//         }
+//     },{scope: 'user_photos'} );
+// }
+// function getAlbums( callback ) {
+//     FB.api(
+//         '/me/albums',
+//         {fields: 'id,cover_photo'},
+//         function(albumResponse) {
+//             //console.log( ' got albums ' );
+//             if (callback) {
+//                 callback(albumResponse);
+//             }
+//         }
+//     );
+// }
+// function getPhotosForAlbumId( albumId, callback ) {
+//     FB.api(
+//         '/'+albumId+'/photos',
+//         {fields: 'id'},
+//         function(albumPhotosResponse) {
+//             //console.log( ' got photos for album ' + albumId );
+//             if (callback) {
+//                 callback( albumId, albumPhotosResponse );
+//             }
+//         }
+//     );
+// }
+// function getLikesForPhotoId( photoId, callback ) {
+//     FB.api(
+//         '/'+albumId+'/photos/'+photoId+'/likes',
+//         {},
+//         function(photoLikesResponse) {
+//             if (callback) {
+//                 callback( photoId, photoLikesResponse );
+//             }
+//         }
+//     );
+// }
+// function getPhotos(callback) {
+//     var allPhotos = [];
+//     var accessToken = '';
+//     login(function(loginResponse) {
+//         accessToken = loginResponse.authResponse.accessToken || '';
+//         getAlbums(function(albumResponse) {
+//             var i, album, deferreds = {}, listOfDeferreds = [];
+//             for (i = 0; i < albumResponse.data.length; i++) {
+//                 album = albumResponse.data[i];
+//                 deferreds[album.id] = $.Deferred();
+//                 listOfDeferreds.push( deferreds[album.id] );
+//                 getPhotosForAlbumId( album.id, function( albumId, albumPhotosResponse ) {
+//                     var i, facebookPhoto;
+//                     for (i = 0; i < albumPhotosResponse.data.length; i++) {
+//                         facebookPhoto = albumPhotosResponse.data[i];
+//                         allPhotos.push({
+//                             'id'	:	facebookPhoto.id,
+//                             'added'	:	facebookPhoto.created_time,
+//                             'url'	:	makeFacebookPhotoURL( facebookPhoto.id, accessToken )
+//                         });
+//                     }
+//                     deferreds[albumId].resolve();
+//                 });
+//             }
+//             $.when.apply($, listOfDeferreds ).then( function() {
+//                 if (callback) {
+//                     callback( allPhotos );
+//                 }
+//             }, function( error ) {
+//                 if (callback) {
+//                     callback( allPhotos, error );
+//                 }
+//             });
+//         });
+//     });
+// }
--- react__tutorial/src/actions/UserActions.js
+++ react__tutorial/src/actions/UserActions.js
@@ -1,4 +1,5 @@
-/* eslint-disable */
+/*global FB:true*/
+
 import {
     LOGIN_REQUEST,
     LOGIN_SUCCES,
@@ -6,81 +7,26 @@ import {
 } from '../constants/User'
 
 export function handleLogin() {
-
     return function(dispatch) {
         dispatch({
             type: LOGIN_REQUEST
         });
 
-        FB.login(function(response) {
-            if (response.authResponse) {
-                console.log('Welcome!  Fetching your information.... ');
-                FB.api('/me', function(response) {
-                    debugger;
-                    console.log('Good to see you, ' + response.name + '.');
-                });
+        FB.login(function(res) {
+            if (res.authResponse) {
+                FB.api('/me', (resUser) => {
+                    dispatch({
+                        type: LOGIN_SUCCES,
+                        payload: resUser.name
+                    });
+                }, {scope: 'user_photos'} );
+
             } else {
-                console.log('User cancelled login or did not fully authorize.');
+                dispatch({
+                    type: LOGIN_FAIL,
+                    payload: new Error('Ошибка авторизации')
+                });
             }
         });
-
-        // FB.getLoginStatus(function(response) {
-        //     if (response.status === 'connected') {
-        //         debugger;
-        //
-        //         dispatch({
-        //             type: LOGIN_SUCCES,
-        //             payload: 'username'
-        //         })
-        //     }
-        //
-        //     else {
-        //         FB.login();
-        //
-        //         dispatch({
-        //             type: LOGIN_FAIL,
-        //             error: true,
-        //             payload: new Error('Ошибка авторизации')
-        //         });
-        //     }
-        // });
-
-        // FB.getLoginStatus(function(response) { // eslint-disable-line
-        //     if (response.status === 'connected') {
-        //         console.log('Logged in.');
-        //         dispatch({
-        //             type: LOGIN_SUCCES,
-        //             payload: 'username'
-        //         })
-        //
-        //     }
-        //     else {
-        //         FB.login();
-        //
-        //         dispatch({
-        //             type: LOGIN_FAIL,
-        //             error: true,
-        //             payload: new Error('Ошибка авторизации')
-        //         });
-        //     }
-        // });
-
-        // FB.Auth.login((r) => { // eslint-disable-line no-undef
-        //     if (r.session) {
-        //         let username = r.session.user.first_name;
-        //
-        //         dispatch({
-        //             type: LOGIN_SUCCES,
-        //             payload: username
-        //         })
-        //
-        //     } else {
-        //         dispatch({
-        //             type: LOGIN_FAIL,
-        //             error: true,
-        //             payload: new Error('Ошибка авторизации')
-        //         })
-        //     }
-        // },4);
     }
 }
