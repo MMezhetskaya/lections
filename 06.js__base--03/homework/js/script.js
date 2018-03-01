e5b948305cd2c91f439d1f3cb2013a5f2bcb6978
Alec Povolotskiy <aleksey.povolotskiy@vaimo.com>
2018-03-01 21:24:25 +0200

    example l 6

--- 06.js__base--03/homework/js/script.js
+++ 06.js__base--03/homework/js/script.js
@@ -21,3 +21,72 @@
  */
 
 'use strict';
+
+function getUserData(title, pHolder) {
+    prompt(title, pHolder);
+}
+
+function setUsersNum() {
+    var usersNum = +prompt('Какое кол-во пользователей вы хотите добавить?', '0-999'),
+        result = document.getElementById('result'),
+        usersData, getSortKey, sortedData;
+
+    if (usersNum) {
+        usersData = setUsersInfo(usersNum);
+        getSortKey = prompt('по какому полю(ключу) сортировать', 'name, age, job');
+        sortedData = usersData.sort(
+            function(a, b) {
+                if (a[getSortKey]) {
+                    return (a[getSortKey] === b[getSortKey])
+                        ? 0
+                        : (a[getSortKey] < b[getSortKey])
+                            ? -1
+                            : 1;
+                }
+            }
+        );
+
+        var sortedMapData = sortedData.map(function (el) {
+            return el.name + ' Age: ' + el.age + el.job;
+        });
+        console.log(sortedMapData);
+        result.innerHTML = sortedMapData.join('<br>');
+    }
+}
+
+function setUsersInfo(usersNum) {
+    var usersData = [];
+
+    for (var i = 1; i <= usersNum; i++) {
+        var userInfo = prompt('User ' + i + ' info', 'имя, возвраст, профессия'),
+            userInfoMap;
+
+        userInfoMap = userInfo
+            .split(',')
+            .reduce(function (prevVal, targetVal, j) {
+                switch (j) {
+                    case 0:
+                        prevVal.name = targetVal;
+                        break;
+
+                    case 1:
+                        prevVal.age = +targetVal;
+                        break;
+
+                    case 2:
+                        prevVal.job = targetVal;
+                        break;
+                }
+
+                return prevVal;
+            }, {});
+
+        usersData.push(userInfoMap)
+    }
+    // debugger;
+    return usersData;
+}
+
+function sortByKey(key) {
+    debugger;
+}
