9f3a3055dd67a5f9b6dd8c1dda0b4782e8acc809
Aleksey Povolotskiy <a.povolotskiy@gmail.com>
2018-03-31 10:49:41 +0300

    l21  final react

--- examples/react__tutorial--01/src/containers/AuthenticatedComponent/index.js
+++ examples/react__tutorial--01/src/containers/AuthenticatedComponent/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { ROUTING } from '../../constants/Routing';
 
 export default function requireAuthentication(Component) {
     class AuthenticatedComponent extends Component {
+        componentWillMount() {
+            this.checkAuth(this.props.user);
+        }
+
+        componentWillReceiveProps(nextProps) {
+            this.checkAuth(nextProps.user);
+        }
+
+        checkAuth(user) {
+            if (!user.isAuthenticated) {
+                this.props.dispatch({
+                    type: ROUTING,
+                    payload: {
+                        method: 'replace',
+                        nextUrl: '/login'
+                    }
+                })
+            }
+        }
+
         render() {
             return (
                 this.props.user.isAuthenticated === true
