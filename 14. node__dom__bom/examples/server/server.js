d370f3d786ce6be2df8cc4c67305d63eb332a766
Alec Povolotskiy <aleksey.povolotskiy@vaimo.com>
2018-04-17 16:55:00 +0300

    l23-24 update

---
+++ 14. node__dom__bom/examples/server/server.js
@@ -0,0 +1,30 @@
+let http = require('http'),
+    static = require('node-static'),
+    file = new static.Server('.'),
+    port = 8080;
+
+http
+    .createServer(
+        (req, res) => {
+            file.serve(req, res);
+
+            res.writeHead(
+                200,
+                {
+                    'Content-Type': 'text/html',
+                    'Cache-Control': 'no-cache'
+                }
+            );
+
+            res.write('<h1>Im Title</h1>');
+            res.end('<p>OK</p>');
+        }
+    )
+    .listen(
+        port,
+        e => {
+            if (e) return console.error(e);
+
+            console.info(`==> 🌎 Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`)
+        }
+    );
