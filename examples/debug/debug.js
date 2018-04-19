9c240c3ba8e63806a5a666c70821a8bd89d632e6
Aleksey Povolotskiy <a.povolotskiy@gmail.com>
2018-04-19 23:10:08 +0300

    l14 syntax update, l15 move examples

--- examples/debug/debug.js
+++
@@ -1,9 +0,0 @@
-function printArgs() {
-    arguments.join = [].join; // одолжили метод (1)
-
-    var argStr = arguments.join(':'); // (2)
-
-    console.log(argStr); // сработает и выведет 1:2:3
-}
-
-printArgs(1, 2, 3);
